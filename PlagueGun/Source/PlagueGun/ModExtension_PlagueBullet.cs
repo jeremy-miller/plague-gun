6d8d9e4d5c3c2a3c4745ac8ef27f9ea3f83b2ad2
Jeremy Miller <jeremymiller@fastmail.com>
2021-03-14 11:15:50 -0700

    Add C# and XML code

---
+++ PlagueGun/Source/PlagueGun/ModExtension_PlagueBullet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace JM_PlagueGun
+{
+    public class ModExtension_PlagueBullet : DefModExtension
+    {
+        public float addHediffChance = 0.05f;
+        public HediffDef hediffToAdd;
+    }
+}
